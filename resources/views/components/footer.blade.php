eeacff5f038892c04179fa1033f3832c92a95bee
Giorgia Marcantonio <giorgia.marcantonio@outlook.it>
2025-05-10 18:27:26 +0200

    scaffolding

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,80 @@
+<!-- Footer -->
+<footer class="bg-body-tertiary text-center">
+    <div class="container p-4">
+        <section class="mb-4 row justify-content-center">
+            <div class="col-6 d-flex justify-content-between align-items-center">
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                    <i class="bi bi-facebook text-center"></i>
+                </a>
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                    <i class="bi bi-twitter-x  text-center"></i>
+                </a>
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                    <i class="bi bi-instagram  text-center"></i>
+                </a>
+            </div>
+           
+        </section>
+
+
+        <!-- Section: Form -->
+        <section class="row justify-content-center align-items-center">
+            <div class="col-8">
+                <form action="">
+                <!--Grid row-->
+                <div class="row d-flex justify-content-center">
+                <!--Grid column-->
+                <div class="col-auto">
+                    <p class="pt-2">
+                    <strong>Sign up for our newsletter</strong>
+                    </p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-5 col-12">
+                    <!-- Email input -->
+                    <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="email" id="form5Example24" class="form-control" />
+                    <label class="form-label" for="form5Example24">Email address</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-auto">
+                    <!-- Submit button -->
+                    <button data-mdb-ripple-init type="submit" class="btn btn-outline mb-4">
+                    Subscribe
+                    </button>
+                </div>
+                <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </form>
+            </div>
+            
+        </section>
+        <!-- Section: Form -->
+
+        <!-- Section: Text -->
+        <section class="mb-4">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+            repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum
+            harum corrupti dicta, aliquam sequi voluptate quas.
+        </p>
+        </section>
+        <!-- Section: Text -->
+
+    </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2020 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
